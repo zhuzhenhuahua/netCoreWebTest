ade5e13960f77aa9f522593e082e6ff05cbb8533
zzh <865729986@qq.com>
2020-06-10 11:44:17 +0800

    测试一下

---
+++ netCoreWebTest/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace netCoreWebTest.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        public string aaa() {
+            return "hello word";
+        }
+    }
+}
