abfa9e82b190e0459da2b039fa11935bbf836f82
zhuzhenhuahua <865729986@qq.com>
2020-06-11 14:57:02 +0800

    基础

---
+++ Core.IRepository/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using SqlSugar;
+
+namespace Core.IRepository
+{
+    public interface IUnitOfWork
+    {
+        SqlSugarClient GetDbClient();
+
+        void BeginTran();
+
+        void CommitTran();
+        void RollbackTran();
+    }
+}
---
+++ Core.IRepository/Interface/IsysUserInfoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Model.Models;
+
+namespace Core.IRepository.Interface
+{
+    /// <summary>
+	/// IsysUserInfoRepository
+	/// </summary>	
+	public interface IsysUserInfoRepository : IBaseRepository<sysUserInfo>//类名
+    {
+
+
+    }
+}
